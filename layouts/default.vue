bc4fbb347e4b18ef1ff1aab8870498c942a82b0d
Xavier Costas <xavicp7@gmail.comm>
2023-01-11 19:28:25 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+      <NuxtLink to="/about">
+        About
+      </NuxtLink>
+    </nav>
+
+    <slot />
+
+  </div>
+</template>
+<script>export default {
+  components: {}
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <nav>
+      <h3>
+      My Anime Tracker is a web app that you can use to track all the animes that you watched and that you want to watch eventually
+      </h3>
+    </nav>
+  </div>
+</template>
